1581f77c2c94e1a643115cfb9905bce17b77c747
Satyam <satyamthakur594@gmail.com>
2024-08-28 23:00:10 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/main/java/com/RestaurantApp/restaurantListing/entity/Restaurant.java
@@ -0,0 +1,13 @@
+package com.RestaurantApp.restaurantListing.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Restaurant {
+}
